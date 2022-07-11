0b12e0f8a50226e1aee5a1c8f810358dc4168815
Chase Martinez <chasemartinez22@gmail.com>
2022-07-11 09:26:51 -0500

    Feat: Add new lol.jsp for LoLServlet to redirect to

--- src/main/java/LolServlet.java
+++ src/main/java/LolServlet.java
@@ -1,17 +1,15 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "LolServlet", urlPatterns = "/lol")
 public class LolServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println(("<h1>L O L<h1>"));
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/lol.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/lol.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chasemartinez
+  Date: 7/11/22
+  Time: 9:23 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>LOL</title>
+</head>
+<body>
+<h1>LOL!</h1>
+</body>
+</html>
