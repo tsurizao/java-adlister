eaf838b0717b60d56d1d5d55e9b8c56d3353c949
Chase Martinez <chasemartinez22@gmail.com>
2022-07-11 09:36:00 -0500

    Feat: Add greeting.jsp for the GreetingServlet to forward information to

--- src/main/java/GreetingServlet.java
+++ src/main/java/GreetingServlet.java
@@ -1,18 +1,17 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "GreetingServlet", urlPatterns = "/greeting")
 public class GreetingServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println(("<h1>Howdy " + name + "<h1>"));
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("/greeting.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/greeting.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chasemartinez
+  Date: 7/11/22
+  Time: 9:27 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Greeting</title>
+</head>
+<body>
+<h1>Howdy, ${name}</h1>
+</body>
+</html>
