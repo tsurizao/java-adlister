01fb180e6fdc9d81d372fc8a6d2f327a8fcbef0b
Chase Martinez <chasemartinez22@gmail.com>
2022-07-12 14:56:21 -0500

    Added one more quote and author to test file

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -9,18 +9,21 @@ public class BeanTest {
         Author me = new Author(2, "me", "myself");
         Author shakespeare = new Author(3, "William", "Shakespeare");
         Author blake = new Author(4, "William", "Blake");
+        Author gump = new Author(5, "Forrest", "Gump");
 
 
         Quote inspirationalQuote = new Quote(1, me, "You can do it!");
-        Quote dadsQuote = new Quote(1, dad, "Are ya winnin' son?");
-        Quote shakespeareQuote = new Quote(1, shakespeare, "Life's but a walking shadow, a poor player, that struts and frets his hour upon the stage, and then is heard no more.  It is a tale told by an idiot, full of sound and fury, signifying nothing.");
-        Quote blakeQuote = new Quote(1, blake, "To see a World in a Grain of Sand...");
+        Quote dadsQuote = new Quote(2, dad, "Are ya winnin' son?");
+        Quote shakespeareQuote = new Quote(3, shakespeare, "Life's but a walking shadow, a poor player, that struts and frets his hour upon the stage, and then is heard no more.  It is a tale told by an idiot, full of sound and fury, signifying nothing.");
+        Quote blakeQuote = new Quote(4, blake, "To see a World in a Grain of Sand...");
+        Quote gumpQuote = new Quote(5, gump, "Life is like a box of chocolates, you never know what you're gonna get.");
 
         ArrayList<Quote> someQuotes = new ArrayList<>();
         someQuotes.add(inspirationalQuote);
         someQuotes.add(dadsQuote);
         someQuotes.add(shakespeareQuote);
         someQuotes.add(blakeQuote);
+        someQuotes.add(gumpQuote);
 
         for (Quote quote : someQuotes){
             System.out.println(quote.getContent() + "\n - " + quote.getAuthor().getFirstName() + " " + quote.getAuthor().getLastName());
