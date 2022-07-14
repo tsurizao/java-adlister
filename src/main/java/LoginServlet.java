b85d29812da0164b55f45e8300820b489874e4e9
Chase Martinez <chasemartinez22@gmail.com>
2022-07-14 14:33:11 -0500

    Refactored to improve scrutinization of user and password to specifically admin and password

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,7 +3,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -11,17 +10,17 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String user = (String) request.getSession().getAttribute("user");
         String password = (String) request.getSession().getAttribute("password");
-        if (user == null || user.trim().equals("") || password == null || password.trim().equals("")) {
+        if (user == null) {
             request.getRequestDispatcher("/login.jsp").forward(request, response);
-        } else {
+        } else if (user.equals("admin") && password.equals("password")){
             response.sendRedirect("/profile");
         }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String user = (String) request.getParameter("user");
-        String password = (String) request.getParameter("password");
-        boolean validUser = user != null && password != null && !user.trim().equals("") && !password.trim().equals("");
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+        boolean validUser = (user.equals("admin") && password.equals("password"));
         if (validUser) {
             request.getSession().setAttribute("user", user);
             request.getSession().setAttribute("password", password);
