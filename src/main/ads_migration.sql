be7598a47ed3f46fefd3af4a6f81e6c8bb39e227
Chase Martinez <chasemartinez22@gmail.com>
2022-07-15 16:20:14 -0500

    Feat: Add users_migration.sql and ads_migration.sql

---
+++ src/main/ads_migration.sql
@@ -0,0 +1,14 @@
+USE adlister_db;
+
+CREATE TABLE IF NOT EXISTS ads (
+                                     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                     user_id INT UNSIGNED NOT NULL,
+                                     title VARCHAR(200) NOT NULL,
+                                     description TEXT NOT NULL,
+                                     PRIMARY KEY (id),
+                                     FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DESCRIBE ads;
+
+SELECT * FROM ads;
