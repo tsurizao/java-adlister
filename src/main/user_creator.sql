fcd9c1a03f7d3a31acceb1fb4a29eed9235d4e2c
Chase Martinez <chasemartinez22@gmail.com>
2022-07-18 15:35:42 -0500

    Refactor: combined migration files into one file for users table and ads table

--- src/main/user_creator.sql
+++ src/main/user_creator.sql
@@ -4,9 +4,13 @@ CREATE USER IF NOT EXISTS 'adlister_db_user'@'localhost' IDENTIFIED BY 'password
 
 GRANT ALL ON adlister_db.* TO 'adlister_db_user'@'localhost';
 
-SELECT user, host FROM mysql.user;
+SELECT user, host
+FROM mysql.user;
 
-SELECT * FROM users;
+SELECT *
+FROM users;
 
-INSERT INTO users (username, email, password) VALUES ('User', 'user@email.com', 'password');
-INSERT INTO users (username, email, password) VALUES ('Another_User', 'anotheruser@email.com', 'password');
+INSERT INTO users (username, email, password)
+VALUES ('User', 'user@email.com', 'password');
+INSERT INTO users (username, email, password)
+VALUES ('Another_User', 'anotheruser@email.com', 'password');
