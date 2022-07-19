1f66c53db2412f72b752f212cebb2ae696b580e3
Chase Martinez <chasemartinez22@gmail.com>
2022-07-19 16:49:20 -0500

    Feat completion of steps 1 and 2 from exercise

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,9 +30,7 @@ public class LoginServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
-
-        boolean validAttempt = password.equals(user.getPassword());
-
+        boolean validAttempt = BCrypt.checkpw(password, user.getPassword());
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
