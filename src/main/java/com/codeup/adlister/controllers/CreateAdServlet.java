6c4f675803189d5f88323ba5d85c1c40e1fe96b4
Chase Martinez <chasemartinez22@gmail.com>
2022-07-20 11:22:17 -0500

    Refactored the create ads servlet to only allow viewing if logged in

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -14,16 +14,17 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        if(user == null){
+            response.sendRedirect("/login");
+            return;
+        }
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
             .forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User user = (User) request.getSession().getAttribute("user");
-        if(user == null){
-            response.sendRedirect("/login");
-            return;
-        }
         Ad ad = new Ad(
             user.getId(), // for now we'll hardcode the user id
             request.getParameter("title"),
